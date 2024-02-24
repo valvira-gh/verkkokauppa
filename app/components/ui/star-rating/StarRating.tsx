884b2638a5510decae1c1e0c56781d50d5c54229
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-02-24 03:10:24 +0200

    StarRating component

---
+++ app/components/ui/star-rating/StarRating.tsx
@@ -0,0 +1,9 @@
+import styles from "./star-rating.module.css";
+
+const StarRating: React.FC = () => {
+  // code
+
+  return <div className={styles.starRating}>{/*  */}</div>;
+};
+
+export default StarRating;
